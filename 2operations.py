3235829413a57769489317f5eb419738ebf3981d
Garvit Tyagi <154688764+garv098@users.noreply.github.com>
2025-07-18 11:42:14 +0530

    Add files via upload

---
+++ 2operations.py
@@ -0,0 +1,51 @@
+#operation are of 4 type 
+# 1 Arithmetic operation
+#addition 
+a= 3
+b= 4
+add= a+b
+print(add)
+
+#subtraction
+a= 4
+b= 6
+sub= a-b
+print(sub)
+
+#multiplication
+
+a= 90
+b= 67
+multi= a*b
+print(multi)
+
+#division
+
+a=70
+b=7
+divid = a/b
+print(divid)
+
+#reminder
+
+a=78
+b=7
+rem = a%b
+print(rem)
+
+#2 relation operation
+#==(equal to)
+#!=(not equal to)
+# <>
+
+#3 assingment operation 
+# =
+# +=
+# -=
+# /=
+#**
+
+#4 logical operation
+# not 
+# and #or they bisically used in boolien 
+
